9a22bb0147662c360ad102a5779acd61dc43fa53
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-05-09 18:30:20 +0530

    Finished the logic for project requirements
    - Turned grid creation code into a function
    - Added random color generator
    - Button now uses grid maker function to create custom grid
    - Refactored the code

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
 'use strict';
 
+// Initial Variable Declaration
+
+let totalPixels = 256;
+let squaresPerSide = 16;
+// let opacity = 0.1;
+
 // Query Selectors
 
 const cell = document.createElement("div");
 const container = document.querySelector(".container");
 const button = document.querySelector("button");
 
-for (let i = 0; i < 256; i++) {
-    const cloneCell = cell.cloneNode(cell);
-    cloneCell.classList.add("cell");
-    container.appendChild(cloneCell);
+// Random Color
 
-    cloneCell.addEventListener("mouseover", () => {
-        cloneCell.style.background = "black";
-    });
+let randColorNum = () => Math.floor(Math.random() * 256);
+// let color = (opacity) => `rgb(${randColorNum()}, ${randColorNum()}, ${randColorNum()}, ${opacity})`;
+let color = `rgb(${randColorNum()}, ${randColorNum()}, ${randColorNum()})`;
+
+// Function to build the grid
+
+function gridMaker(pixelCount, squaresEachSide) {
+    for (let i = 0; i < pixelCount; i++) {
+        const cloneCell = cell.cloneNode(cell);
+        cloneCell.classList.add("cell");
+        cloneCell.style.width = `${960 / squaresEachSide}px`;
+        cloneCell.style.height = `${960 / squaresEachSide}px`;
+        container.appendChild(cloneCell);
+
+        // cloneCell.dataset.hoverCount = 0;
+        cloneCell.addEventListener("mouseenter", (event) => {
+            // let hoverCount = parseInt(event.target.dataset.hoverCount);
+            // event.target.dataset.hoverCount = ++hoverCount;
+            // let newOpacity = Math.min(0.1 * hoverCount, 1);
+
+            // cloneCell.style.background = color(newOpacity);
+            cloneCell.style.background = color;
+            // console.log(color(newOpacity));
+        });
+    }
 }
 
+gridMaker(totalPixels, squaresPerSide);
+
+// Custom grid button
+
 button.addEventListener("click", () => {
-    const squaresPerSide = +prompt("Enter Squares Per Side")
-    console.log(squaresPerSide);
 
-})
+    do {
+        squaresPerSide = +prompt("Enter Squares Per Side (under 100)");
+    } while (isNaN(squaresPerSide) || squaresPerSide > 100);
+    
+    container.innerHTML = '';
 
+    totalPixels = squaresPerSide ** 2;
+
+    gridMaker(totalPixels, squaresPerSide);
+
+});
