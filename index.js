bc661c3386e5c0ad3d6f6a91ad6d9e3cfcda7931
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-05-09 15:48:19 +0530

    Added logic for flexbox grid and mouse hover effect

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// Query Selectors
+
+const cell = document.createElement("div");
+const container = document.querySelector(".container");
+const button = document.querySelector("button");
+
+for (let i = 0; i < 256; i++) {
+    const cloneCell = cell.cloneNode(cell);
+    cloneCell.classList.add("cell");
+    container.appendChild(cloneCell);
+
+    cloneCell.addEventListener("mouseover", () => {
+        cloneCell.style.background = "black";
+    });
+}
+
+button.addEventListener("click", () => {
+    const squaresPerSide = +prompt("Enter Squares Per Side")
+    console.log(squaresPerSide);
+
+})
+
